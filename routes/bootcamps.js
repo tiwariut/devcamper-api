d0b0ec0ddf7121745f1f4c3846350cd965b804bd
Utkarsh Tiwari <u.tiwari@huulke.com>
2020-09-10 19:26:14 +0530

    Add mongoose advanced querying and relationships

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -4,12 +4,31 @@ const {
   getBootcamp,
   createBootcamp,
   updateBootcamp,
-  deleteBootcamp
+  deleteBootcamp,
+  getBootcampsInRadius,
+  bootcampPhotoUpload
 } = require('../controllers/bootcamps');
+
+const Bootcamp = require('../models/Bootcamp');
+
+const advancedResults = require('../middleware/advancedResults');
+
+// Include other resource routers
+const courseRouter = require('./courses');
 
 const router = express.Router();
 
-router.route('/').get(getBootcamps).post(createBootcamp);
+// Re-route into other resource routers
+router.use('/:bootcampId/courses', courseRouter);
+
+router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
+
+router.route('/:id/photo').put(bootcampPhotoUpload);
+
+router
+  .route('/')
+  .get(advancedResults(Bootcamp, 'courses'), getBootcamps)
+  .post(createBootcamp);
 
 router
   .route('/:id')
